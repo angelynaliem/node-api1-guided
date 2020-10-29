59565ed4e6b3ea7173882bfbfa09d0265fb7bb7b
angelynaliem <angelynaliem@gmail.com>
2020-10-28 22:24:31 -0400

    TL Q&A

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express")
-const shortid = require("shortid")
+const express = require("express");
+const shortid = require("shortid");
 
-let hubs = []
+let hubs = [];
 
-const port = 5000
+const port = 5000;
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 // const server = http.createServer((req, res) => {
 //     res.statusCode = 200
@@ -15,75 +15,72 @@ server.use(express.json())
 //     res.end("Hello World, from NodeJS")
 // })
 
-
 server.get("/", (req, res) => {
-    res.json({hello: "World!"})
-})
+  res.json({ hello: "World!" });
+});
 
 server.get("/hello", (req, res) => {
-    res.json({hello: "Lambda!"})
-})
+  res.json({ hello: "Lambda!" });
+});
 
 //create
 server.post("/api/hubs", (req, res) => {
-    const hubInfo = req.body
-    hubInfo.id = shortid.generate()
-    hubs.push(hubInfo)
+  const hubInfo = req.body;
+  hubInfo.id = shortid.generate();
+  hubs.push(hubInfo);
 
-    res.status(201).json(hubInfo)
-
-})
+  res.status(201).json(hubInfo);
+});
 
 //read
 server.get("/api/hubs", (req, res) => {
-    res.status(200).json(hubs)
-})
+  res.status(200).json(hubs);
+});
 
-//delete 
+//delete
 server.delete("/api/hubs/:id", (req, res) => {
-    const {id} = req.params
-    const deleted = hubs.find(hub => hub.id === id)
+  const { id } = req.params;
+  const deleted = hubs.find((hub) => hub.id === id);
 
-    if(deleted) {
-        hubs = hubs.filter(hub => hub.id !== id)
-        res.status(200).json(deleted)
-    } else {
-        res.status(404).json({ message: "id not found" })
-    }
-})
+  if (deleted) {
+    hubs = hubs.filter((hub) => hub.id !== id);
+    res.status(200).json(deleted);
+  } else {
+    res.status(404).json({ message: "id not found" });
+  }
+});
 
 //update / patch
-server.put("/api/hubs/:id", (req,res) => {
-    const {id} = req.params
-    const changes = req.body
-    changes.id = id
+server.put("/api/hubs/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
-    let index = hubs.findIndex(hub => hub.id === id)
+  let index = hubs.findIndex((hub) => hub.id === id);
 
-    if (index !== -1) {
-    hubs[index] = changes 
-    res.status(200).json(index, changes)
-    } else {
-        res.status(404).json({ message: "id not found" })
-    }
-
-})
+  if (index !== -1) {
+    changes.id = id;
+    hubs[index] = changes;
+    // res.status(200).json(index, changes)
+    res.json(index, changes);
+  } else {
+    res.status(404).json({ message: "id not found" });
+  }
+});
 
 server.patch("/api/hubs/:id", (req, res) => {
-    const {id} = req.params
-    const changes = req.body
+  const { id } = req.params;
+  const changes = req.body;
 
-    let found = hubs.find(hub => hub.id === id)
+  let found = hubs.find((hub) => hub.id === id);
 
-    if(found) {
-        Object.assign(found, changes)
-        res.status(200).json(found)
-    } else {
-        res.status(404).json({ message: "id not found" })
-    }
-
-})
+  if (found) {
+    Object.assign(found, changes);
+    res.status(200).json(found);
+  } else {
+    res.status(404).json({ message: "id not found" });
+  }
+});
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
